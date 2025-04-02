18596fbf49efab7116b1af4d718deb61a660de46
Shariq Choudhary <khanshariq92213@gmail.com>
2025-04-02 18:33:48 +0530

    minor change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'your-image-service.com'],
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
   },
 };
 

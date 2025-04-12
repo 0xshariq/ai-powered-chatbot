b1451df3bd3974c3421b8b0b74d753b1ffa66bd5
Shariq Choudhary <khanshariq92213@gmail.com>
2025-04-12 22:39:36 +0530

    minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 
 const geistSans = Geist({
@@ -33,7 +32,6 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Toaster position="top-right" />
-        <Footer />
       </body>
     </html>
   );
